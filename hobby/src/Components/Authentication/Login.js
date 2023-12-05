978168a6370abd883ae62cb1f92d20b4eff319ae
GowryB <gowry.bofficial@gmail.com>
2023-12-05 11:43:20 +0530

    Add login validations

--- hobby/src/Components/Authentication/Login.js
+++ hobby/src/Components/Authentication/Login.js
@@ -1,23 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 
-function Login() {
+import Form from "react-bootstrap/Form";
+
+import Button from "react-bootstrap/Button";
+
+import "./Login.css";
+
+export default function Login() {
+  const [UserName, setUserName] = useState("");
+
+  const [Password, setPassword] = useState("");
+
+  function validateForm() {
+    return UserName.length > 0 && password.length > 0;
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
+
   return (
-    <div className="dflex">
-      <div className="container">
-        <form>
-          <label>
-            UserName:
-            <input type="text" />
-          </label>
+    <div className="Login">
+      <Form onSubmit={handleSubmit}>
+        <Form.Group size="lg" controlId="email">
+          <Form.Label>UserName</Form.Label>
 
-          <label>
-            Password:
-            <input type="text" />
-          </label>
-        </form>
-      </div>
+          <Form.Control
+            autoFocus
+            type="UserName"
+            value={UserName}
+            onChange={(e) => setUserName(e.target.value)}
+          />
+        </Form.Group>
+
+        <Form.Group size="lg" controlId="password">
+          <Form.Label>Password</Form.Label>
+
+          <Form.Control
+            type="password"
+            value={Password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </Form.Group>
+
+        <Button block size="lg" type="submit" disabled={!validateForm()}>
+          Login
+        </Button>
+      </Form>
     </div>
   );
 }
-
-export default Login;
